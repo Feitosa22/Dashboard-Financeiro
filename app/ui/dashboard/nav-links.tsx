f6f1a8d7d57e0ae92d9f55a309a91f946f53243e
Feitosa22 <111542071+Feitosa22@users.noreply.github.com>
2024-07-20 12:00:28 -0300

    Version 0.101

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -3,7 +3,7 @@
 import {
   UserGroupIcon,
   HomeIcon,
-  InboxIcon,
+  DocumentDuplicateIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -17,7 +17,7 @@ const links = [
   {
     name: "Invoices",
     href: "/dashboard/invoices",
-    icon: InboxIcon,
+    icon: DocumentDuplicateIcon,
   },
   { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
 ];
--- versions.rd
+++ versions.rd
@@ -1,2 +1,10 @@
-heroicons
-npm install @heroicons/react
+Adicionar cadastro de usuários!
+Adicionar checagem no envio do email
+ou
+Adicionar confirmação de email para enviar email
+ou Estar logado para enviar email
+Adicionar Metricas!
+
+// https://www.tremor.so/
+// https://www.chartjs.org/
+// https://airbnb.io/visx/
