c6bc708cd1bb376a3bdfc3e85d66b9fa02d9f6af
Feitosa22 <111542071+Feitosa22@users.noreply.github.com>
2024-07-22 00:45:57 -0300

    Corrigido bug 41

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,23 +1,29 @@
 import { Metadata } from 'next';
 import Table from '@/app/ui/customers/table';
 import { fetchFilteredCustomers } from "@/app/lib/data";
+import { Suspense } from "react";
  
 export const metadata: Metadata = {
   title: 'Clientes',
 };
 
 
-export default async function Page({ searchParams,
+export default async function Page({ 
+  searchParams,
 }: {
   searchParams?: {
     query?: string;
     page?: string;
   };
 }) {
-  const customers = await fetchFilteredCustomers();
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const customers = await fetchFilteredCustomers(query);
   return(
     <>
-    <Table customers={customers}/>
+    <Suspense key={customers[0].email} >
+        <Table customers={customers}/>
+      </Suspense>
     </>
   )
 }
